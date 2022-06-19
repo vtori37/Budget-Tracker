960f950b7c6ec21115e2ab88224552f0d175e2b6
Tori Acer <vrice360@gmail.com>
2022-06-19 12:01:22 -0500

    Update

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,20 +2,20 @@ const APP_PREFIX = 'budget-tracker-cache-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./css/style.css",
-  "./js/idb.js",
-  "./js/index.js",
-  "./icons/icon-72x72.png",
-  "./icons/icon-96x96.png",
-  "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-192x192.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png",
-  "./manifest.json"
-
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/idb.js",
+  "/js/index.js",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png"
+  // "/manifest.json"
 ];
 
 // Respond with cached resources
